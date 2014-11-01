47d8768f8473ee784921d2d15b48f755ee073cf4
Eric Ho <eric.ho@yale.edu>
2014-11-01 11:08:42 -0400

    added basic chat search funcitonality

--- test.js
+++ test.js
@@ -13,47 +13,14 @@ function click_element(element) {
 	dispatchMouseEvent(element, 'mouseup', true, true);
 };
 
-// alert("hit this file")
-
-document.addEventListener("DOMContentLoaded", function() {
-    alert("hi");
-});
-
-// var dispatchKeyboardEvent = function(target, initKeyboradEvent_args) {
-//   var e = document.createEvent("KeyboardEvents");
-//   e.initKeyboardEvent.apply(e, Array.prototype.slice.call(arguments, 1));
-//   target.dispatchEvent(e);
-// };
-// var dispatchTextEvent = function(target, initTextEvent_args) {
-//   var e = document.createEvent("TextEvent");
-//   e.initTextEvent.apply(e, Array.prototype.slice.call(arguments, 1));
-//   target.dispatchEvent(e);
-// };
-// var dispatchSimpleEvent = function(target, type, canBubble, cancelable) {
-//   var e = document.createEvent("Event");
-//   e.initEvent.apply(e, Array.prototype.slice.call(arguments, 1));
-//   target.dispatchEvent(e);
-// };
-
-// var canceled = !dispatchKeyboardEvent(element,
-//     'keydown', true, true,  // type, bubbles, cancelable
-//     null,  // window
-//     'h',  // key
-//     0, // location: 0=standard, 1=left, 2=right, 3=numpad, 4=mobile, 5=joystick
-//     '');  // space-sparated Shift, Control, Alt, etc.
-// dispatchKeyboardEvent(
-//     element, 'keypress', true, true, null, 'h', 0, '');
-// if (!canceled) {
-//   if (dispatchTextEvent(element, 'textInput', true, true, null, 'h', 0)) {
-//     element.value += 'h';
-//     dispatchSimpleEvent(element, 'input', false, false);
-//     // not supported in Chrome yet
-//     // if (element.form) element.form.dispatchFormInput();
-//     dispatchSimpleEvent(element, 'change', false, false);
-//     // not supported in Chrome yet
-//     // if (element.form) element.form.dispatchFormChange();
-//   }
-// }
-// dispatchKeyboardEvent(
-//     element, 'keyup', true, true, null, 'h', 0, '');
+document.onscroll = function() {
+	var searchbox = document.getElementsByClassName('inputtext inputsearch textInput');
+	click_element(searchbox[0]);
+	// button_press(document,'h');
+	searchbox[0].value = 'kevin jiang';
+	// button_press_eric(13);
+	var keyEvent = document.createEvent('KeyboardEvent');
+	keyEvent.initKeyboardEvent('keydown', true, false, null, 0, false, 0, false, 13, 0);
+	searchbox[0].dispatchEvent(keyEvent);
+};
 
