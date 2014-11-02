ac511ba668d71c7f5d004e196f6013961ca7c98b
diego <diegoalbertotorres@gmail.com>
2014-11-01 21:24:52 -0400

    Almost ready to graph

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,34 +1,34 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.method == "setLocalStorage"){
+		chrome.storage.local.users = request.data;
+		sendResponse({});
+		//~console.log("Data received:", chrome.storage.local.users);
+		
+		// Update data
+		
+		console.log("ATorres:");
+		console.log(chrome.storage.local.users["5dd3d67b"]);
+		
+	}
+});
+
 $(function () {
  options = {
-        data: {
-            csv: document.getElementById('csv').innerHTML
-        },
-
+        
         chart: {
             type: 'heatmap',
-            inverted: true
+            marginTop: 40,
+            marginBottom: 40
         },
 
 
         title: {
-            text: 'Highcharts heat map study',
-            align: 'left'
-        },
-
-        subtitle: {
-            text: 'Temperature variation by day and hour through April 2013',
-            align: 'left'
+            text: 'Sales per employee per weekday'
         },
 
         xAxis: {
-            tickPixelInterval: 50,
-            min: Date.UTC(2013, 3, 1),
-            max: Date.UTC(2013, 3, 30)
-        },
-
-        yAxis: {
-            title: {
-                text: null
+			title: {
+                text: "Hour"
             },
             labels: {
                 format: '{value}:00'
@@ -43,27 +43,48 @@ $(function () {
             max: 23
         },
 
+        yAxis: {
+            categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+            title: null
+        },
+
         colorAxis: {
-            stops: [
-                [0, '#3060cf'],
-                [0.5, '#fffbbc'],
-                [0.9, '#c4463a']
-            ],
-            min: -5
+            min: 0,
+            minColor: '#FFFFFF',
+            maxColor: Highcharts.getOptions().colors[0]
+        },
+
+        legend: {
+            align: 'right',
+            layout: 'vertical',
+            margin: 0,
+            verticalAlign: 'top',
+            y: 25,
+            symbolHeight: 320
+        },
+
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> sold <br><b>' +
+                    this.point.value + '</b> items on <br><b>' + this.series.yAxis.categories[this.point.y] + '</b>';
+            }
         },
 
         series: [{
-            borderWidth: 0,
-            colsize: 24 * 36e5, // one day
-            tooltip: {
-                headerFormat: 'Temperature<br/>',
-                pointFormat: '{point.x:%e %b, %Y} {point.y}:00: <b>{point.value} ℃</b>'
+            name: 'Sales per employee',
+            borderWidth: 1,
+            // Data is an array of arrays. Each element has form: [Hour, day, value]
+            data: [[0,1,10],[0,1,19],[0,2,8],[0,3,24],[0,4,67],[1,0,92],[1,1,58],[1,2,78],[1,3,117],[1,4,48],[2,0,35],[2,1,15],[2,2,123],[2,3,64],[2,4,52],[3,0,72],[3,1,132],[3,2,114],[3,3,19],[3,4,16],[4,0,38],[4,1,5],[4,2,8],[4,3,117],[4,4,115],[5,0,88],[5,1,32],[5,2,12],[5,3,6],[5,4,120],[6,0,13],[6,1,44],[6,2,88],[6,3,98],[6,4,96],[7,0,31],[7,1,1],[7,2,82],[7,3,32],[7,4,30],[8,0,85],[8,1,97],[8,2,123],[8,3,64],[8,4,84],[9,0,47],[9,1,114],[9,2,31],[9,3,48],[9,4,91]],
+            dataLabels: {
+                enabled: true,
+                color: 'black',
+                style: {
+                    textShadow: 'none'
+                }
             }
         }]
 
-	};
-	
-	console.log(chrome.storage.local.users);
+    };
 	
 	$('#container').highcharts(options);
 });
